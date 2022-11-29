77bd33e98d87e79041753ecc3180ea06f05f6ed0
Tomás Salina <salinatomass53@gmail.com>
2022-11-29 19:56:17 -0300

    Header responsive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -17,19 +17,19 @@ export const Header = ({ onSearch }: HeaderProps) => {
   }, 1500)
 
   return (
-    <header className="max-w-7xl w-full m-auto flex items-center justify-between py-4 px-6">
+    <header className="max-w-7xl w-full m-auto flex items-center justify-center lg:justify-between py-4 px-6">
       <div className="h-fit flex items-center">
         <figure className="w-fit p-3 hover:bg-gray-200 rounded-full cursor-pointer">
           <Logo />
         </figure>
         <button
-          className="h-full py-3 px-5 bg-black text-white font-bold rounded-3xl"
+          className="hidden lg:block h-full py-3 px-5 bg-black text-white font-bold rounded-3xl"
           type="button"
         >
           Home
         </button>
       </div>
-      <div className="flex items-center gap-2 w-3/5 bg-gray-200 rounded-full py-4 px-5">
+      <div className="flex items-center gap-2 w-4/5 lg:w-3/5 bg-gray-200 rounded-full py-4 px-5">
         <SearchIcon className="w-6 h-6 text-gray-500" />
         <input
           className="w-full bg-transparent outline-0 placeholder:text-gray-500"
@@ -39,7 +39,7 @@ export const Header = ({ onSearch }: HeaderProps) => {
           onChange={handleSearch}
         />
       </div>
-      <div className="flex items-center">
+      <div className="hidden lg:flex items-center">
         <button className="w-fit p-3 hover:bg-gray-200 rounded-full cursor-pointer">
           <NotificationIcon className="w-7 h-7 text-gray-500" />
         </button>

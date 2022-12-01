ff91bc304763106b85e33a2c421c4c2e6b02ba08
Tomás Salina <salinatomass53@gmail.com>
2022-12-01 16:43:45 -0300

    Change error server message

--- src/utils/errMessage.ts
+++ src/utils/errMessage.ts
@@ -1,5 +1,6 @@
 export const errMessage = {
-  errorServer: 'We are having problems, please try again later.',
+  errorServer:
+    'We exceeded the maximum number of requests, please try again later.',
   notFound: "We couldn't find any Pins for this search.",
   notTooMany: 'There are not too many results.',
 }
